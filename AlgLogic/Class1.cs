b75540602f24257e8091f28a2c392ea21db00089
DaniilPrikhoddko <d1912dd@gmail.com>
2024-09-19 23:24:00 +0500

    Начал писать матрицы

--- AlgLogic/Class1.cs
+++ AlgLogic/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using MatrixEntities;
+using System.Diagnostics;
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
 
@@ -11,28 +12,40 @@ namespace AlgLogic
     public class Test
     {
         public AlgorithmItnerface Instance { set; get; }
-        int rangeOfRandomNumbers{ set; get; }
+        int rangeOfRandomNumbers { set; get; }
         int vectorLength { set; get; }
         int numberOfStarts { set; get; }
-        float[] vector {  set; get; }
+
+        int powNumber { set; get; }
+        float[] vector { set; get; }
         public Test(AlgorithmItnerface Instance, int rangeOfRandomNumbers, int vectorLength, int numberOfStarts)
         {
             this.Instance = Instance;
             this.rangeOfRandomNumbers = rangeOfRandomNumbers;
             this.vectorLength = vectorLength;
             this.numberOfStarts = numberOfStarts;
-            vector = new Vector(vectorLength, rangeOfRandomNumbers).GenerateRandomVector(); 
+            vector = new Vector(vectorLength, rangeOfRandomNumbers).GenerateRandomVector();
+        }
+
+        public Test(AlgorithmItnerface Instance, int rangeOfRandomNumbers, int vectorLength, int numberOfStarts, int powNum)
+        {
+            this.Instance = Instance;
+            this.rangeOfRandomNumbers = rangeOfRandomNumbers;
+            this.vectorLength = vectorLength;
+            this.numberOfStarts = numberOfStarts;
+            vector = new Vector(vectorLength, rangeOfRandomNumbers).GenerateRandomVector();
+            this.powNumber = powNum;
         }
         public float[] StartAlgorithm()
         {
-            List<float> points = new List<float> ();
+            List<float> points = new List<float>();
             float algorithmExecutionTime;
             for (int i = 0; i < numberOfStarts; i++)
             {
                 for (int j = 0; j < vectorLength; j++)
                 {
-                    Timer timer = new Timer(Instance, vector);
-                    algorithmExecutionTime = timer.CalculateTime();
+                    Timer timer = new Timer(Instance);
+                    algorithmExecutionTime = timer.CalculateTime((float[])vector.Take(j));
                     points.Add(algorithmExecutionTime);
                 }
                 points.Add(0); // Максон, смотри если ты встречаешь ноль то ты дорисовал график и надо не удаляя текущий начать рисовать следующий поверх
@@ -54,12 +67,12 @@ namespace AlgLogic
         }
         public float[] GenerateRandomVector()
         {
-            float[] vector = new float[vectorLength];
+            float[] vector = new float[vectorLength]; //перепишите пж чтобы вектор был интовым и генерился на рандомных интах
             Random random = new Random();
 
             for (int i = 0; i < vectorLength; i++)
             {
-                vector[i] = (float) (random.NextDouble() * rangeOfRandomNumbers); 
+                vector[i] = (float)(random.NextDouble() * rangeOfRandomNumbers);
             }
 
             return vector;
@@ -86,7 +99,7 @@ namespace AlgLogic
                 int k = rng.Next(n + 1);
                 float value = vector[k];
                 vector[k] = vector[n];
-                vector[n] = value;               
+                vector[n] = value;
             }
             return vector;
         }
@@ -94,14 +107,12 @@ namespace AlgLogic
 
     public class Timer
     {
-        float[] vector { set; get; }
         public AlgorithmItnerface Instance { set; get; }
-        public Timer(AlgorithmItnerface Instance, float[] vector) 
+        public Timer(AlgorithmItnerface Instance)
         {
             this.Instance = Instance;
-
         }
-        public float CalculateTime()
+        public float CalculateTime(float[] vector)
         {
             Stopwatch stopwatch = new Stopwatch();
 
@@ -118,9 +129,10 @@ namespace AlgLogic
             float elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
             return elapsedMilliseconds;
         }
+
     }
 
-    public class Algorithm1: AlgorithmItnerface
+    public class Algorithm1 : AlgorithmItnerface
     {
         float[] vector { set; get; }
         public Algorithm1(float[] vector)
@@ -172,10 +184,10 @@ namespace AlgLogic
         }
     }
 
-    public class AlgorithmBuble : AlgorithmItnerface
+    public class AlgorithmBubbleSort : AlgorithmItnerface
     {
         float[] vector { set; get; }
-        public AlgorithmBuble(float[] vector)
+        public AlgorithmBubbleSort(float[] vector)
         {
             this.vector = vector;
         }
@@ -198,10 +210,10 @@ namespace AlgLogic
             return 0;
         }
     }
-    public class AlgorithmQuick : AlgorithmItnerface
+    public class AlgorithmQuickSort : AlgorithmItnerface
     {
         float[] vector { set; get; }
-        public AlgorithmQuick(float[] vector)
+        public AlgorithmQuickSort(float[] vector)
         {
             this.vector = vector;
         }
@@ -241,15 +253,18 @@ namespace AlgLogic
     public class AlgorithmQuickPow : AlgorithmItnerface
     {
         float[] vector { set; get; }
-        public AlgorithmQuickPow(float[] vector)
+        int n { get; set; }
+        public AlgorithmQuickPow(float[] vector, int n)
         {
             this.vector = vector;
+            this.n = n;
+
         }
-        public void QuickPow (int x, int n)
+        public void QuickPow(int x, int n)
         {
             int c = x;
             int k = n;
-            int f; 
+            int f;
 
             if (k % 2 == 1)
             {
@@ -269,12 +284,138 @@ namespace AlgLogic
                 {
                     f = f * c;
                 }
-            }                         
+            }
         }
         public int ExecuteAlgorithm(float[] vector)
         {
-            QuickPow();
+            for (int x = 0; x < vector.Length; x++)
+            {
+                QuickPow(x, n);
+            }
             return 0;
         }
     }
+
+    public class AlgorithmQuickPow2 : AlgorithmItnerface
+    {
+        public float[] vector { set; get; }
+        int n { set; get; }
+        public AlgorithmQuickPow2(float[] vector, int n)
+        {
+            this.vector = vector;
+            this.n = n;
+        }
+        public void QuickPow2(int x, int n)
+        {
+            int c = x;
+            int f = 1;
+            int k = n;
+            while (k != 0)
+            {
+                if (k % 2 == 0)
+                {
+                    c = c * c;
+                    k = k % 2;
+                }
+
+                else
+                {
+                    f = f * c;
+                    k = k - 1;
+                }
+
+            }
+
+        }
+
+        public int ExecuteAlgorithm(float[] vector)
+        {
+            for (int x = 0; x < vector.Length; x++)
+            {
+                QuickPow2(x, n);
+            }
+            return 1;
+        }
+    }
+
+    public class AlgorithmRecPow : AlgorithmItnerface
+    {
+        public float[] vector { set; get; }
+        int n { set; get; }
+        public AlgorithmRecPow(float[] vector, int n)
+        {
+            this.vector = vector;
+            this.n = n;
+        }
+        public int RecPow(int x, int n)
+        {
+            int f;
+            if (n == 0)
+            {
+                f = 1;
+            }
+
+            else
+            {
+                f = RecPow(x, n / 2);
+                if (n % 2 == 1)
+                {
+                    f = f * f * x;
+                }
+
+                else
+                {
+                    f = f * f;
+                }
+
+            }
+
+            return f;
+        }
+
+        public int ExecuteAlgorithm(float[] vector)
+        {
+            for (int x = 0; x < vector.Length; x++)
+            {
+                RecPow(x, n);   
+            }
+
+            return 1;
+        }
+
+    }
+
+    public class AlgorithmClassicPow : AlgorithmItnerface
+    {
+        public float[] vector { set; get; }
+        int n { set; get; }
+        public AlgorithmClassicPow(float[] vector, int n)
+        {
+            this.vector = vector;
+            this.n = n;
+        }
+        public void ClassicPow(int x, int n)
+        {
+            int f = 1;
+            int k = 0;
+            while (k < n)
+            {
+                f = f * x;
+                k = k + 1;
+            }
+
+        }
+
+        public int ExecuteAlgorithm(float[] vector)
+        {
+            for (int x = 0; x < vector.Length; x++)
+            {
+                ClassicPow(x, n);
+            }
+
+            return 1;
+        }
+
+    }
+
 }
