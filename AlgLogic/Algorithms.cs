e7b2e65ac4c1442c84db54c1af0ded2306f8cba1
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-21 20:07:56 +0500

    добавил алгоритм тимсорт бок 1

--- AlgLogic/Algorithms.cs
+++ AlgLogic/Algorithms.cs
@@ -331,6 +331,76 @@ namespace AlgLogic
             return 1;
         }
 
+        public class AlgorithmTimSort : AlgorithmItnerface
+        {
+            public int[] vector { set; get; }
+            private const int RUN = 32;
+
+            public static void InsertionSort(int[] arr, int left, int right)
+            {
+                for (int i = left + 1; i <= right; i++)
+                {
+                    int temp = arr[i];
+                    int j = i - 1;
+                    while (j >= left && arr[j] > temp)
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                    }
+                    arr[j + 1] = temp;
+                }
+            }
+
+            public static void Merge(int[] arr, int left, int mid, int right)
+            {
+                int len1 = mid - left + 1, len2 = right - mid;
+                int[] leftArr = new int[len1];
+                int[] rightArr = new int[len2];
+
+                Array.Copy(arr, left, leftArr, 0, len1);
+                Array.Copy(arr, mid + 1, rightArr, 0, len2);
+
+                int i = 0, j = 0, k = left;
+                while (i < len1 && j < len2)
+                {
+                    if (leftArr[i] <= rightArr[j])
+                        arr[k++] = leftArr[i++];
+                    else
+                        arr[k++] = rightArr[j++];
+                }
+
+                while (i < len1)
+                    arr[k++] = leftArr[i++];
+
+                while (j < len2)
+                    arr[k++] = rightArr[j++];
+            }
+
+            public static void TimSortAlgorithm(int[] arr, int n)
+            {
+                // Сортировка каждого маленького подмассива вставками
+                for (int i = 0; i < n; i += RUN)
+                    InsertionSort(arr, i, Math.Min(i + RUN - 1, n - 1));
+
+                // Слияние подмассивов
+                for (int size = RUN; size < n; size = 2 * size)
+                {
+                    for (int left = 0; left < n; left += 2 * size)
+                    {
+                        int mid = left + size - 1;
+                        int right = Math.Min((left + 2 * size - 1), (n - 1));
+
+                        if (mid < right)
+                            Merge(arr, left, mid, right);
+                    }
+                }
+            }
+            public int ExecuteAlgorithm(int[] vector)
+            {  
+                TimSortAlgorithm(vector, vector.Length);
+                return 0;
+            }
+        }
     }
 
 }
