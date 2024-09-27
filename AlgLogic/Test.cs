11a15e52b5ae96ad5c548c9773f43ea84bfe2bbb
choye1 <makcim233237@gmail.com>
2024-09-27 23:17:20 +0500

    its 1/2 alive!

--- AlgLogic/Test.cs
+++ AlgLogic/Test.cs
@@ -29,8 +29,9 @@
                     Timer timer = new Timer(Instance);
                     algorithmExecutionTime = timer.CalculateTime(vector.Take(j).ToArray());
                     points.Add(algorithmExecutionTime);
-                    string path = Path.Combine(Environment.CurrentDirectory, algorithmName);
-                    File.WriteAllText(path, algorithmExecutionTime.ToString());
+                    //string path = Path.Combine(Environment.CurrentDirectory, algorithmName);                    
+                    //File.WriteAllText(path, algorithmExecutionTime.ToString());
+                    
                 }
 
                 points.Add(0); // Максон, смотри если ты встречаешь ноль то ты дорисовал график и надо не удаляя текущий начать рисовать следующий поверх
