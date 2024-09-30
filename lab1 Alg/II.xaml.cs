bb6724f1f065dd24f39a31dc63c6345433ec594a
choye1 <makcim233237@gmail.com>
2024-09-30 23:41:12 +0500

    начало матриц

--- lab1 Alg/II.xaml.cs
+++ lab1 Alg/II.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ScottPlot;
+﻿using MatrixEntities;
+using ScottPlot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+ 
 
 namespace lab1_Alg
 {
@@ -23,15 +25,71 @@ namespace lab1_Alg
         public II()
         {
             InitializeComponent();
-            Plot plot = new Plot();
-            List<int> dataX = new List<int>();
-            List<int> dataY = new List<int>();
-            Graph2.Plot.Add.Scatter(dataX, dataY);
         }
 
         private void ClearPlot(object sender, RoutedEventArgs e)
         {
             Graph2.Plot.Clear();
+            Graph2.Refresh();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int b = (int) SlMarixSize.Value;
+            int a = (int)SlMaxVal.Value;
+            int c = (int)SlCountStart.Value;
+            List<float> dataX = new List<float>();
+            MatrixTest matrixTest = new MatrixTest(a, b, c);
+            float[] result = matrixTest.StartAlgorithm();
+            for (int i = 0; i<=result.Length; i++) { dataX.Add(i) ; }
+            Output(Slise(result),dataX);
+
+        }
+        private void Output(List<List<float>> resultList, List<float> dataX)
+        {
+            string[] numGraphs = TbNumGraph2.Text.Split(',');
+            foreach (var i in numGraphs)
+            {
+                if (i.Split('-').Length == 2)
+                {
+                    for (int j = Convert.ToInt32(i.Split('-')[0]); j <= Convert.ToInt32(i.Split("-")[1]); j++)
+                    {
+                        List<float> dataY = resultList[j].ToList();
+                        Graph2.Plot.Add.Scatter(dataX, dataY);
+                        Graph2.Refresh();
+                    }
+                }
+
+                else
+                {
+                    List<float> dataY = resultList[int.Parse(i)].ToList();
+                    Graph2.Plot.Add.Scatter(dataX, dataY);
+                    Graph2.Refresh();
+
+                }
+            }
+        }
+
+        private List<List<float>> Slise(float[] result)
+        {
+            int i = 0;
+            List<List<float>> resultList = new List<List<float>>() { };
+            resultList.Add(new List<float>());
+            foreach (var item in result)
+            {
+                if (item == -1)
+                {
+                    i++;
+                    resultList.Add(new List<float>());
+                }
+
+                else
+                {
+                    resultList[i].Add(item);
+                }
+            }
+
+            return resultList;
         }
     }
 }
