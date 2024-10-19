c0b8868c5e83390cea8e6b8b9d0c60aeda4e686d
choye1 <makcim233237@gmail.com>
2024-10-19 23:30:50 +0500

    .

--- AlgLogic/obj/Debug/net7.0/AlgLogic.AssemblyInfo.cs
+++ AlgLogic/obj/Debug/net7.0/AlgLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6d4beaa8f5235f7f009415031436f83a9009cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edba9b94a64fd99d766d15f013d47050d8dbd573")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
